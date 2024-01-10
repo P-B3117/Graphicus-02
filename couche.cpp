c2ef0dba8a7fa10b7274ccaa91ce17fadc398976
ornitagou <ornitagou@gmail.com>
2024-01-10 10:18:44 -0500

    vroom vroom

--- couche.cpp
+++ couche.cpp
@@ -10,4 +10,41 @@
 
 #include "couche.h"
 
-// Implementation a faire...
+using namespace std;
+
+Couche()
+{
+	state = 0;
+}
+~Couche()
+{
+}
+
+bool addForme(Forme* forme)
+{
+	return vecteur.add(forme);
+}
+
+Forme* removeForme(int index)
+{
+	return vecteur.remove
+}
+Forme* getForme(int index)
+{
+	return vecteur.getShape(index);
+}
+bool translateCouche(int x, int y)
+{
+	int grandeur;
+	grandeur = vecteur.getSize();
+	Forme forme;
+	
+	for(int i = 0; i < grandeur; i++)
+	{
+		forme = vecteur.getShape(i);
+		
+	}
+}
+bool reinitialise();
+bool stateChange(int state);
+void afficher(ostream flot);
